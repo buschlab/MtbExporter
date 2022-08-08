92a5feb68df5505c37360a21324e872eb869da5f
Niklas Reimer <n.reimer@uni-luebeck.de>
2022-08-08 17:21:35 +0200

    cleanup

--- src/main/java/de/uzl/lied/mtbexporter/settings/Settings.java
+++ src/main/java/de/uzl/lied/mtbexporter/settings/Settings.java
@@ -3,7 +3,6 @@ package de.uzl.lied.mtbexporter.settings;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.File;
-import java.util.List;
 
 /**
  * Settings for MtbExporter.
@@ -18,23 +17,18 @@ public class Settings {
     private static Integer cronIntervall;
     @JsonProperty("fhir")
     private static FhirSettings fhir;
-    @JsonProperty("regex")
-    private static List<Regex> regex;
 
     /**
      * Initializes settings object.
      * @param outputFolder
      * @param cronIntervall
      * @param fhir
-     * @param regex
      */
     public Settings(@JsonProperty("outputFolder") File outputFolder,
-            @JsonProperty("cronIntervall") Integer cronIntervall, @JsonProperty("fhir") FhirSettings fhir,
-            @JsonProperty("regex") List<Regex> regex) {
+            @JsonProperty("cronIntervall") Integer cronIntervall, @JsonProperty("fhir") FhirSettings fhir) {
         Settings.outputFolder = outputFolder;
         Settings.cronIntervall = cronIntervall;
         Settings.fhir = fhir;
-        Settings.regex = regex;
     }
 
     public static File getOutputFolder() {
@@ -47,10 +41,6 @@ public class Settings {
 
     public static FhirSettings getFhir() {
         return fhir;
-    }
-
-    public static List<Regex> getRegex() {
-        return regex;
     }
 
 }
